3ff2d58ca4bf96ade8ea213164a45f42209091f0
pablofedericosantillan <pablofedericosantillan@gmail.com>
2021-08-05 07:16:36 -0300

    Desafio Clase 26

---
+++ api/function_passport.js
@@ -0,0 +1,70 @@
+//-LOGIN
+/*function postLogin (req, res) {
+  //var user = req.user;
+  res.sendFile(process.cwd() + '/public/index.html');
+}*/
+function postLogin (req, res) {
+  let { username } = req.body
+  req.session.username = username
+  res.sendFile(process.cwd() + '/public/index.html');
+}
+
+function getLogin(req, res) {  
+  if (req.isAuthenticated()) {
+    //console.log('user logueado',req.user);
+    res.render('tablas', {
+      nombre: req.user.username
+    });
+  }
+  else {
+    console.log('user NO logueado');
+    res.sendFile(process.cwd() + '/public/login.html');
+  }
+}
+
+
+function getFaillogin (req, res) {
+  console.log('error en login');
+  res.render('login-error', {
+  });
+}
+
+//-SIGNUP
+function postSignup (req, res) {
+  var user = req.user;
+  res.sendFile(process.cwd() + '/public/signup.html');
+}
+
+function getSignup(req, res) {
+    res.sendFile(process.cwd() + '/public/signup.html');
+}
+
+function getFailsignup (req, res) {
+  console.log('error en signup');
+  res.render('signup-error', {
+  });
+}
+
+function getSignupSucessfull (req, res) {
+  req.logout();
+  res.sendFile(process.cwd() + '/public/login.html');
+
+}
+
+//-LOGOUT
+function getLogout (req, res) {
+  let nombre = req.user.username;
+  req.logout();
+  res.render("logout", {nombre})
+}
+
+module.exports = {
+    getLogin,
+    postLogin,
+    getFaillogin,
+    getLogout,
+    getSignup,
+    postSignup,
+    getFailsignup,
+    getSignupSucessfull
+}
--- server.js
+++ server.js
@@ -6,24 +6,7 @@ const productos = require('./api/productos');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-
-const MongoStore = require('connect-mongo')
-const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-
-app.use(cookieParser())
-app.use(session({
-    store: MongoStore.create({
-        mongoUrl: "mongodb+srv://pfsantillan:35783028@cluster0.kfxor.mongodb.net/ecommerce?retryWrites=true&w=majority",
-        mongoOptions: advancedOptions,
-        ttl: 10*60
-    }),
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false,
-}))
+app.use(express.static('public'))
 
 /* -------------------- Base de Datos ---------------------- */
 require('./baseDatos/models/coneccion');
@@ -33,12 +16,64 @@ const baseDatosMensajes = require('./baseDatos/baseDatosMensajes');
 const productosRouter = require('./routes/productos');
 app.use('/api', productosRouter);
 
+/* ------------------- PASSPORT ---------------------------- */
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bCrypt = require('./api/bCrypt');
+const User = require('./baseDatos/baseDatosUsers');
 
-const handlebars = require('express-handlebars')
+passport.use('login', new LocalStrategy({
+  passReqToCallback: true
+},
+  async function (req, username, password, done) {
+    // Verificacion si en la BD existe el usuario
+    let u = await User.listar();
+    let usuario = u.find(u => u.username === username);
+    if (!usuario) {
+        return done(null, false, console.log('mensaje', 'usuario no encontrado'));
+    } else {
+        if (!bCrypt.isValidPassword(usuario, password)) {
+            console.log('contraseña invalida');
+            return done(null, false, console.log('mensaje', 'contraseña invalida'));
+        } else {
+            return done(null, usuario);
+        }
+    }
+})
+);
 
 
-//--------------------------------------------
-//establecemos la configuración de handlebars
+passport.use('signup', new LocalStrategy({
+    passReqToCallback: true
+}, async function (req, username, password, done) {
+    // Verificacion si en la BD existe el usuario
+    let u = await User.listar();
+    let usuario = u.find(u => u.username === username);
+    if (usuario) {
+        console.log('usuario ya existe');
+        return done(null, false, console.log('mensaje', 'usuario ya existe'));
+    } else {
+        let newU = {
+            username: username,
+            password: bCrypt.createHash(password),
+        };
+        let newUser = await User.guardar(newU);
+
+        return done(null, newUser,console.log('Resgitro de Usuarios Exitoso!!!'));
+    }
+})
+);
+
+passport.serializeUser(function (user, done) {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser(async function (id, done) {
+    done(null, await User.buscarPorId(id));
+  });
+
+/* --------------- Configuración de handlebars -------------- */
+const handlebars = require('express-handlebars');
 app.engine(
     "hbs",
     handlebars({
@@ -48,45 +83,50 @@ app.engine(
 );
 app.set("view engine", "hbs");
 app.set("views", "./views");
-//--------------------------------------------
 
-app.use(express.static('public'))
+/* -----------------Cookies, Session y Storages--------------------- */
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')
+const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
-/* -------------------------------------------------------- */
-/* -------------------------------------------------------- */
+app.use(cookieParser())
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://localhost:27017/ecommerce',//"mongodb+srv://pfsantillan:35783028@cluster0.kfxor.mongodb.net/ecommerce?retryWrites=true&w=majority",
+        mongoOptions: advancedOptions,
+        ttl: 10*60
+    }),
+   /* cookie: {
+        httpOnly: false,
+        secure: false,
+        maxAge: 10*10*60
+    },*/
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+}));
 
+//Inicializamos Passport 
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/login', (req,res) => {
-    if(req.session.nombre) {
-        res.render("tablas", {
-            nombre: req.session.nombre
-        })
-    }
-    else {
-        res.sendFile(process.cwd() + '/public/login.html')
-    }
-})
+/* -----------------ENDPOINTS: LOGING, SIGNUP Y LOGOUT--------------------- */
+const function_passport = require('./api/function_passport');
 
-app.post('/login', (req,res) => {
-    let { nombre } = req.body
-    req.session.nombre = nombre
-    res.redirect('/')
-})
+//-LOGIN
+app.post('/login', passport.authenticate('login', { failureRedirect: '/faillogin' }), function_passport.postLogin);
+app.get('/login', function_passport.getLogin);
+app.get('/faillogin', function_passport.getFaillogin);
 
-app.get('/logout', (req,res) => {
-    let nombre = req.session.nombre
-    if(nombre) {
-        req.session.destroy( err => {
-            if(!err) res.render("logout", { nombre })
-            else res.redirect('/')
-        })
-    }
-    else {
-        res.redirect('/')
-    }
-})
-/* -------------------------------------------------------- */
+//-SIGNUP
+app.post('/signup', passport.authenticate('signup', { failureRedirect: '/failsignup' }), function_passport.postSignup);
+app.get('/signup', function_passport.getSignup);
+app.get('/signup-exitoso', function_passport.getSignupSucessfull);
+app.get('/failsignup', function_passport.getFailsignup);
 
+//  LOGOUT
+app.get('/logout', function_passport.getLogout);
 
 /* -------------------- Web Sockets ---------------------- */
 const mensajes = [];
@@ -105,6 +145,7 @@ io.on('connection', async socket => {
         io.sockets.emit('messages', await baseDatosMensajes.leer()); 
     })    
 });
+
 /* ------------------------------------------------------- */
 /* ------------------------------------------------------- */
 const PORT = process.env.PORT || 8080;
